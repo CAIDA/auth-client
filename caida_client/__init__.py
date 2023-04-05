e762e195087939f6d51c499f7b38ec2cf8d07f25
Ken Keys <kkeys@caida.org>
2023-04-05 15:28:26 -0700

    Add packaging

---
+++ caida_client/__init__.py
@@ -0,0 +1,3 @@
+"""Tools for accessing protected CAIDA services"""
+
+__version__ = "0.1"
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+import caida_client
+
+setuptools.setup(
+    name='caida_client',
+    version=caida_client.__version__,
+    author='Ken Keys',
+    author_email='kkeys@caida.org',
+    description='Access protected CAIDA services',
+    # long_description='file: README.md',
+    # long_description_content_type='text/markdown',
+    # url='https://...',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests_oauthlib',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'get_offline_token=caida_client.get_offline_token:main'
+            'offline_query=caida_client.offline_query:main'
+        ]
+    },
+)
