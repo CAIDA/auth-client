1c859f182faf57b4646541c0546988d7eb2e7856
Ken Keys <kkeys@caida.org>
2023-05-12 13:53:36 -0700

    Log, don't throw, exceptions in save_tokens()

--- caida_sso_client/lib.py
+++ caida_sso_client/lib.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import time
 import json
 
@@ -7,9 +8,14 @@ def make_save_tokens(token_file):
     def save_tokens(token_info):
         if "expires_at" not in token_info and "expires_in" in token_info:
             token_info["expires_at"] = time.time() + token_info["expires_in"]
+        logging.info("### Saving new tokens to %s", token_file)
         oldmask = os.umask(0o077)
-        with open(token_file, "w") as f:
-            json.dump(token_info, f)
+        try:
+            with open(token_file, "w") as f:
+                json.dump(token_info, f)
+        except OSError as e:
+            logging.warning("Failed to save new tokens: %s: %s",
+                    type(e).__name__, str(e))
         os.umask(oldmask)
 
     return save_tokens
